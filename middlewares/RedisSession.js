0c1fb0f6054db655f0715c4493467807ca882752
steve xu <stevexucityu@gmail.com>
2016-05-25 01:22:32 -0700

    setup jsdoc for RedisSession

--- middlewares/RedisSession.js
+++ middlewares/RedisSession.js
@@ -1,15 +1,16 @@
+"use strict";
 /**
  *
  * RedisSession middleware -- created for practice
- * @param {Object}: config - the configuration object (connection, maxAge, sessionId)
- * @param {Object}: config.connection - the connection object for Redis server
- * @param {String}: config.connection.host - the redis host
- * @param {Number}: config.connection.port - the redis port
- * @param {Number}: config.maxAge - the time to live
- * @param {String}: config.sessionId - the key for the session's id in the cookie
+ *
+ * @param {object} config - the configuration object (connection, maxAge, sessionId)
+ * @param {object} config.connection - the connection object for Redis server
+ * @param {string} config.connection.host - the redis host
+ * @param {number} config.connection.port - the redis port
+ * @param {number} config.maxAge - the time to live
+ * @param {string} config.sessionId - the key for the session's id in the cookie
  *
  * @example
- * ```js
  * // initialize the middleware
  * app.use(mySession({
  *   connection: {host: '127.0.0.1', port: 6379}, // Redis连接信息
@@ -18,12 +19,13 @@
  * }));
  *
  * // set/get session variables in req.session
- * ```
+ *
  */
-
-exports = module.exports = (config => {
+function RedisSession(config) {
   return (req, res, next) => {
     console.log();
   };
-});
+}
 
+
+exports = module.exports = RedisSession;
